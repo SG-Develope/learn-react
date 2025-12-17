09060371b00d63fd1c4b7589c843567ce3c7de29
kilyong jeong <uzoolove@gmail.com>
2025-12-17 10:43:57 +0900

    TodoList 라우터 적용

--- workspace-ins/ch12-app/todolist/05-router/src/App.tsx
+++ workspace-ins/ch12-app/todolist/05-router/src/App.tsx
@@ -1,7 +1,31 @@
+import './App.css';
+
 function App(){
   return (
     <>
-      <h1>vite-template</h1>
+      <div className="todoapp">
+        <header>
+          <h1>Todo List</h1>
+          <nav>
+            <div>
+              <ul>
+                <li><a className="menu-dark" href="./home.html">Home</a></li>
+                <li><a className="menu" href="./about.html">About</a></li>
+                <li><a className="menu" href="./todolist.html">TodoList</a></li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <div id="main">
+          <h2>Home</h2>
+          <div className="todo">
+            <p>Todo List App은 할일을 관리하는 리액트 앱입니다.</p>
+          </div>
+        </div>
+        <footer>
+          <p>Copyrightⓒ FrontEnd BootCamp. All rights reserved.</p>
+        </footer>
+      </div>
     </>
   );
 }
---
+++ workspace-ins/ch12-app/todolist/05-router/src/routes.tsx
@@ -0,0 +1,19 @@
+import About from "@/pages/About";
+import Home from "@/pages/Home";
+import TodoAdd from "@/pages/TodoAdd";
+import TodoEdit from "@/pages/TodoEdit";
+import TodoInfo from "@/pages/TodoInfo";
+import TodoList from "@/pages/TodoList";
+import { createBrowserRouter, Navigate } from "react-router";
+
+const router = createBrowserRouter([
+  { path: '/', element: <Navigate to="/home" /> },
+  { path: '/home', element: <Home /> },
+  { path: '/about', element: <About /> },
+  { path: '/todolist', element: <TodoList /> },
+  { path: '/todoadd', element: <TodoAdd /> },
+  { path: '/todoinfo', element: <TodoInfo /> },
+  { path: '/todoedit', element: <TodoEdit /> },
+]);
+
+export default router;
