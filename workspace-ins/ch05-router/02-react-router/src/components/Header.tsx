a261b58eb01b2e4839c779c12931bc5a2755c58c
kilyong jeong <uzoolove@gmail.com>
2025-12-17 10:19:11 +0900

    Navigate 추가

--- workspace-ins/ch05-router/02-react-router/src/components/Header.tsx
+++ workspace-ins/ch05-router/02-react-router/src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <>
       <header>
         <h1>리액트 라우터 - 02 react-router 사용</h1>
-        <NavLink className={({ isActive }) => isActive ? 'menu-dark' : 'memu'} to="/home">home</NavLink>
+        <NavLink className={({ isActive }) => isActive ? 'menu-dark' : 'memu'} to="/">home</NavLink>
         <br/>
         <NavLink className={({ isActive }) => isActive ? 'menu-dark' : 'memu'} to="/page1">page1</NavLink>
         <br/>
