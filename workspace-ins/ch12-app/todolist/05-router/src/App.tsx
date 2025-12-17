986a0ce7390967e362535686ce2270b3d813034c
kilyong jeong <uzoolove@gmail.com>
2025-12-17 10:47:19 +0900

    TodoList 라우터

--- workspace-ins/ch12-app/todolist/05-router/src/App.tsx
+++ workspace-ins/ch12-app/todolist/05-router/src/App.tsx
@@ -1,30 +1,15 @@
+import Header from '@/components/Header';
 import './App.css';
+import Footer from '@/components/Footer';
+import Home from '@/pages/Home';
 
 function App(){
   return (
     <>
       <div className="todoapp">
-        <header>
-          <h1>Todo List</h1>
-          <nav>
-            <div>
-              <ul>
-                <li><a className="menu-dark" href="./home.html">Home</a></li>
-                <li><a className="menu" href="./about.html">About</a></li>
-                <li><a className="menu" href="./todolist.html">TodoList</a></li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-        <div id="main">
-          <h2>Home</h2>
-          <div className="todo">
-            <p>Todo List App은 할일을 관리하는 리액트 앱입니다.</p>
-          </div>
-        </div>
-        <footer>
-          <p>Copyrightⓒ FrontEnd BootCamp. All rights reserved.</p>
-        </footer>
+        <Header />
+        <Home />
+        <Footer />
       </div>
     </>
   );
