142cb9bbab7d9a26728e5ae69e380003443f25c6
kilyong jeong <uzoolove@gmail.com>
2026-01-06 13:04:07 +0900

    generateStaticParams 추가

--- workspace-ins/ch10-nextjs/02-cna/app/posts/[id]/page.tsx
+++ workspace-ins/ch10-nextjs/02-cna/app/posts/[id]/page.tsx
@@ -15,6 +15,17 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
   }
 }
 
+// generateStaticParams 함수가 반환한 배열 만큼 PostInfo 함수를 빌드 시점에 미리 호출함
+// .next/server/app/posts/1.html, 2.html 파일을 정적으로 미리 생성함
+export function generateStaticParams(){
+  const paramsList = [
+    { id: '1' },
+    { id: '2' },
+  ];
+
+  return paramsList;
+}
+
 // 동적 세그먼트의 값을 꺼낼때 params prop을 사용
 export default async function PostInfo({ params }: { params: Promise<{ id: string }> }){
   const { id } = await params;
